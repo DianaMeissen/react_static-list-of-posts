b99676306f2fe336b535ea5838a3f2633c38dc5b
Darya Vyshniakova <vishnya054@gmail.com>
2019-07-05 00:03:33 +0300

    Updates

---
+++ precache-manifest.f6c1b2288dead543bc75ac86a37f587c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801058fb67434816b98c3c310ad4d373",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "de07b9dc0dbc9b664acb",
+    "url": "/react_static-list-of-posts/static/css/main.6b0531a2.chunk.css"
+  },
+  {
+    "revision": "0ce1425da59f8de6f60a",
+    "url": "/react_static-list-of-posts/static/js/2.ea299d33.chunk.js"
+  },
+  {
+    "revision": "de07b9dc0dbc9b664acb",
+    "url": "/react_static-list-of-posts/static/js/main.9fb929b6.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  },
+  {
+    "revision": "a1415513a39d1a9b6e824c39110f911d",
+    "url": "/react_static-list-of-posts/static/media/main-bg.a1415513.png"
+  }
+]);
